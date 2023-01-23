6e5314870f99d531ec574df7a0a6900eaed8bf2b
Sarisol0510 <70375141+Sarisol0510@users.noreply.github.com>
2023-01-23 19:33:38 +0900

    ragdoll modify & pingpong little
    Press "R" in ragdoll state -> ReLoad Scene

--- KingMini/Assets/Team/Insue/script/damage_obj.cs
+++ KingMini/Assets/Team/Insue/script/damage_obj.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using StarterAssets;
 
 public class damage_obj : MonoBehaviour
@@ -11,6 +12,10 @@ public class damage_obj : MonoBehaviour
 
     private bool is_death = false;
 
+
+    [Header("Die on flat")]
+    public bool die_on_flat = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +25,22 @@ public class damage_obj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (die_on_flat==true)
+        {
+            if (transform.position.y < 0)
+            {
+                print("d");
+                Die();
+            }
+        }
+        if (is_death)
+        {
+            if(Input.GetKey(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
     }
 
     private void OnCollisionEnter(Collision other)
@@ -46,14 +66,20 @@ public class damage_obj : MonoBehaviour
     {
         if (is_death == false && other.gameObject.tag == "Obstacle")
         {
-            is_death = true;
-            player_mesh.SetActive(false);
-            player_ragdoll.SetActive(true);
-            controller.GetComponent<ThirdPersonController>().MoveSpeed = 0;
-            controller.GetComponent<ThirdPersonController>().JumpHeight = 0;
-            controller.GetComponent<ThirdPersonController>().RotationSmoothTime = 100;
-            controller.GetComponent<ThirdPersonController>().LockCameraPosition = true;
-            controller.GetComponent<ThirdPersonController>().CinemachineCameraTarget = player_ragdoll;
+            Die();
         }
     }
+
+    public void Die()
+    {
+        is_death = true;
+        player_mesh.SetActive(false);
+        player_ragdoll.SetActive(true);
+        controller.GetComponent<ThirdPersonController>().MoveSpeed = 0;
+        controller.GetComponent<ThirdPersonController>().JumpHeight = 0;
+        controller.GetComponent<ThirdPersonController>().RotationSmoothTime = 100;
+        controller.GetComponent<ThirdPersonController>().LockCameraPosition = true;
+        controller.GetComponent<ThirdPersonController>().CinemachineCameraTarget = player_ragdoll;
+    }
+
 }

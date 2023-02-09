af2a06fe7f9aa6f479cf054d946ee4e00697080f
SYiee <zyx2009@naver.com>
2023-02-10 00:14:45 +0900

    New Character Prefab update

--- KingMini/Assets/Team/Zosteen/MoveToghether.cs
+++ KingMini/Assets/Team/Zosteen/MoveToghether.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class MoveToghether : MonoBehaviour
 {
-
+    public GameObject player;
     private void OnCollisionEnter(Collision collision)
     {
-        //if(collision.gameObject.tag == "Player") 
+        if(collision.gameObject.tag == "Player") 
         {
-            print(collision.gameObject.name);
-            collision.transform.SetParent(transform);
+            player.transform.SetParent(transform);
         }
     }
     private void OnCollisionExit(Collision collision)
     {
-        //if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            collision.transform.SetParent(null);
+            player.transform.SetParent(null);
         }
     }
 }

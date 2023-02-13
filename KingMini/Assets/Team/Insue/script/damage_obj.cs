d2f47dc9f2a3052f8de1fbc33c81066dba30f935
CHERISH61111 <84861196+CHERISH61111@users.noreply.github.com>
2023-02-13 17:06:20 +0900

    Basic UI

--- KingMini/Assets/Team/Insue/script/damage_obj.cs
+++ KingMini/Assets/Team/Insue/script/damage_obj.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Invector.vCharacterController;
+using TMPro;
+
 
 public class damage_obj : MonoBehaviour
 {
@@ -23,10 +26,16 @@ public class damage_obj : MonoBehaviour
 
     [Header("Death Manager")]
     public GameObject death_manager;
+    public TextMeshProUGUI Death_UI;  // 몇 번 죽었는지 표시
 
     private void Awake()
     {
-       death_manager = FindObjectOfType<death_manage>().gameObject;
+        
+        death_manager = FindObjectOfType<death_manage>().gameObject;
+
+        // death UI 할당 & 업데이트
+        Death_UI = GameObject.Find("Death_Num").GetComponent<TextMeshProUGUI>();
+        Death_UI.text = death_manager.transform.GetComponent<death_manage>().death_count.ToString();  
     }
 
     // Update is called once per frame
@@ -49,6 +58,7 @@ public class damage_obj : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 death_manager.transform.GetComponent<death_manage>().death_count++;
                 is_death = false;
+
             }
 
         }
@@ -75,6 +85,7 @@ public class damage_obj : MonoBehaviour
     {
         electric = elec;
     }
+
     public void Die()
     {
         is_death = true;
@@ -89,5 +100,5 @@ public class damage_obj : MonoBehaviour
         //player_controller.GetComponent<vThirdPersonInput>().enabled = false;
         //player_controller.GetComponent<vThirdPersonController>().enabled = false;
         Camera.GetComponent<vThirdPersonCamera>().SetMainTarget(player_ragdoll.transform);
-    }
+    }   
 }
--- KingMini/Assets/Team/Insue/script/death_manage.cs
+++ KingMini/Assets/Team/Insue/script/death_manage.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class death_manage : MonoBehaviour
 {
+
     private void Awake()
     {
         var obj = FindObjectsOfType<death_manage>();
+
         if (obj.Length == 1)
         {
             DontDestroyOnLoad(gameObject);
@@ -16,7 +20,9 @@ public class death_manage : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     public float death_count = 0;
+
     void Update()
     {
         if (Input.GetKey(KeyCode.P))

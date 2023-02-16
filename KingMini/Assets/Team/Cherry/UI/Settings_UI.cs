54e56d1c52aa59d42c1baa3c8791004bb31e885f
CHERISH61111 <84861196+CHERISH61111@users.noreply.github.com>
2023-02-17 01:49:40 +0900

    Add kitchen

---
+++ KingMini/Assets/Team/Cherry/UI/Settings_UI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Settings_UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BacktoGame()
+    {
+        // 다시 게임으로
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+        Cursor.visible = false;
+    }
+
+    public void GotoMainMenu()
+    {
+        // 메인메뉴로 씬 전환
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame() // 게임 종료
+    {
+        Application.Quit();
+    }
+}

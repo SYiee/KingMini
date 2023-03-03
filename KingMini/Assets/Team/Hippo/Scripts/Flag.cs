776fa8cb7962f684d2bf6fe4649556571d22e967
Heokunho <93777460+Heokunho@users.noreply.github.com>
2023-03-03 16:24:42 +0900

    Flag Fix(few change..)

--- KingMini/Assets/Team/Hippo/Scripts/Flag.cs
+++ KingMini/Assets/Team/Hippo/Scripts/Flag.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Flag : MonoBehaviour
 {
     Animation anim;
+
+    private Quaternion rot;
+
     private void Awake()
     {
         anim = GetComponent<Animation>();
@@ -14,6 +17,38 @@ public class Flag : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             anim.Play("FlagAnim");
+        }
+    }
+
+    GameObject player = null;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rot.x = collision.transform.localRotation.x;
+            rot.y = collision.transform.localRotation.y;
+            rot.z = collision.transform.localRotation.z;
+
+            if (player == null)
+                player = collision.gameObject;
+            player.transform.parent.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.rotation = Quaternion.Euler(new Vector3(rot.x, rot.y, rot.z));
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.transform.parent.SetParent(null);
         }
     }
 }
